923e1dc906f095a89d8293512c301fc81ecad48a
Juan Pablo Gomez <jpgomezm@eafit.edu.co>
2024-10-26 21:33:39 -0500

    Error IOS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'intersection-observer'; // Polyfill para mejorar compatibilidad en Safari
 import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import Navbar from './components/Navbar/Navbar';
@@ -10,17 +10,17 @@ import CategoryScreen from './screens/CategoryScreen/CategoryScreen';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  // Para pruebas, cambia a false para verificar si el loader es la causa del problema
   const [loading, setLoading] = useState(true);
-  // Cache busting query string para evitar problemas de cache
-  const Loader = 'https://storage.googleapis.com/comprobantes-madriguera/multimediaFeria/loader-irrelevant.gif?v=1';
+  const Loader = 'https://storage.googleapis.com/comprobantes-madriguera/multimediaFeria/loader-irrelevant.gif?v=2'; // Cambié la versión para forzar cache-busting
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000); // Cambiado a 4000 ms para mostrar el loader durante 4 segundos
+    }, 3000); // Tiempo reducido a 3 segundos
 
-    return () => clearTimeout(timer);
-  }, []);
+    return () => clearTimeout(timer); // Limpieza del temporizador
+  }, []); // Sin dependencias para evitar ciclo de renderización
 
   if (loading) {
     return (
