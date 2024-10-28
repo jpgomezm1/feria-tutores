2cc6b836bacd39ce97f8befb18f25c72778182cd
Juan Pablo Gomez <jpgomezm@eafit.edu.co>
2024-10-27 20:44:27 -0500

    Eliminamos Loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'intersection-observer'; // Polyfill para mejorar compatibilidad en Safari
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -10,34 +10,6 @@ import CategoryScreen from './screens/CategoryScreen/CategoryScreen';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  // Para pruebas, cambia a false para verificar si el loader es la causa del problema
-  const [loading, setLoading] = useState(true);
-  const Loader = 'https://storage.googleapis.com/comprobantes-madriguera/multimediaFeria/loader-irrelevant.gif?v=2'; // Cambié la versión para forzar cache-busting
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); // Tiempo reducido a 3 segundos
-
-    return () => clearTimeout(timer); // Limpieza del temporizador
-  }, []); // Sin dependencias para evitar ciclo de renderización
-
-  if (loading) {
-    return (
-      <div 
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          backgroundColor: '#fff',
-        }}
-      >
-        <img src={Loader} alt="Cargando..." style={{ width: '500px', height: '500px' }} />
-      </div>
-    );
-  }
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
