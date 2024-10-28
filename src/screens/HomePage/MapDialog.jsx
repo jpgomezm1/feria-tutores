e1aa0658581e4de7efa8426f2946867a15b63eb7
Juan Pablo Gomez <jpgomezm@eafit.edu.co>
2024-10-28 08:29:34 -0500

    Mapa Feria Final

--- src/screens/HomePage/MapDialog.jsx
+++ src/screens/HomePage/MapDialog.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Dialog, DialogContent, IconButton, Box } from '@mui/material';
+import { Dialog, DialogContent, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
-
+// Importa la imagen desde la carpeta 'assets'
+import feriaMapa from '../../assets/mapa-feria-final.svg';
 
 function MapDialog({ open, onClose }) {
-  const mapUrl = 'https://storage.googleapis.com/comprobantes-madriguera/multimediaFeria/Feria%20mapa.svg';
-
   return (
     <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
       <IconButton
@@ -16,20 +15,20 @@ function MapDialog({ open, onClose }) {
           position: 'absolute',
           right: 8,
           top: 8,
-          color: 'white', // Color blanco para mejor visibilidad
-          bgcolor: 'rgba(0, 0, 0, 0.5)', // Fondo semi-transparente para contraste
+          color: 'white',
+          bgcolor: 'rgba(0, 0, 0, 0.5)',
           '&:hover': {
-            bgcolor: 'rgba(0, 0, 0, 0.7)', // Acentuar el fondo en hover
+            bgcolor: 'rgba(0, 0, 0, 0.7)',
           },
-          zIndex: 11, // Asegurar que esté al frente
-          fontSize: '1.5rem' // Tamaño del icono
+          zIndex: 11,
+          fontSize: '1.5rem',
         }}
       >
         <CloseIcon fontSize="inherit" />
       </IconButton>
 
       <DialogContent dividers>
-        <img src={mapUrl} alt="Mapa de la Feria" style={{ width: '100%', borderRadius: '8px' }} />
+        <img src={feriaMapa} alt="Mapa de la Feria" style={{ width: '100%', borderRadius: '8px' }} />
       </DialogContent>
     </Dialog>
   );
